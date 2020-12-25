3bf9fb65725e957a34f6940c8e9fbf7673d48d25
DLM <55211307+dlminvestments@users.noreply.github.com>
2020-12-24 22:50:46 -0500

    Create setup.py

---
+++ pythonlib/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+setup(
+    name=’mypythonlib’,
+
+packages=find_packages(include=[‘mypythonlib’]),
+    version=’0.1.0',
+    description=’Python library’,
+    author=’Me’,
+    license=’MIT’,
+
+[options]
+install_requires =
+    pytest==4.4.1’
+    django==1.11.0
+    dj-database-url==0.2.1
+    djangorestframework==2.3.1
+    py==1.8.1’]
+    pip==19.0.3
+    setuptools==40.8.0
+    parse==1.14.0
+    pluggy==0.13.1
+    requests==1.2.0
+    django-model-utils==1.1.0
+    bpython==0.12’
+    tensorflow==2.4.0
+    django-braces==0.2.1
+    django-oauth2-provider==0.2.4
+    attrs==19.3.0
+python_requires = >3.6
+setup_requires =
+    setuptools=>42.0
