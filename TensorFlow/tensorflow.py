50c40265fef7fa855f5702e3ab775bc41694c353
DLM <55211307+dlminvestments@users.noreply.github.com>
2021-01-18 23:10:28 -0500

    Create tensorflow.py

---
+++ TensorFlow/tensorflow.py
@@ -0,0 +1,8 @@
+iterator = training_dataset.make_one_shot_iterator()
+x, y = iterator.get_next()
+
+model.fit(x, y, steps_per_epoch=100, epochs=10)
+
+iterator = validation_dataset.make_one_shot_iterator()
+x, y = iterator.get_next()
+model.evaluate(x, y, steps=50)
